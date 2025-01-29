ade2ff6a4e4c3e7f62fd4a768aa05481d79a575a
Meezaan-ud-Din A Wal-Ikram <m@meezaan.net>
2025-01-29 10:31:08 +0400

    Change order of execution

--- systemd-resolve/execute.sh
+++ systemd-resolve/execute.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+wget -O /tmp/resolved.conf https://raw.githubusercontent.com/meezaan/kubernetes/master/etc/systemd/resolved.conf
+
 apt install systemd-resolved
 
-wget -O /etc/systemd/resolved.conf https://raw.githubusercontent.com/meezaan/kubernetes/master/etc/systemd/resolved.conf
+mv /tmp/resolved.conf /etc/systemd/resolved.conf
 
 systemctl restart systemd-resolved
