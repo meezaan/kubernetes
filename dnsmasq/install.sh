0652a5ac2491c8e78f92fe4f96a4a56ba8561951
Meezaan-ud-Din A Wal-Ikram <m@meezaan.net>
2025-02-09 08:17:17 +0400

    Switch to dnsmasq instead of systemd-resolved

---
+++ dnsmasq/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+apt update
+unlink /etc/resolv.conf
+
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+apt install -y dnsmasq
+systemctl is-enabled dnsmasq
+ cp /etc/dnsmasq.conf{,.orig}
--- systemd-resolve/execute.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-wget -O /tmp/resolved.conf https://raw.githubusercontent.com/meezaan/kubernetes/master/etc/systemd/resolved.conf
-
-apt install systemd-resolved
-
-mv /tmp/resolved.conf /etc/systemd/resolved.conf
-
-systemctl restart systemd-resolved
