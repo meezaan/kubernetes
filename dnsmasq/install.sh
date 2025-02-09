4c6a95398c5705efa4340af3b4aef42a392f09de
مِيزَانُ الْدِّينْ عَبْدُ ذِيْ الْجَلَالِ وَ الْإِكْرَامِ <m@meezaan.net>
2025-02-09 11:04:42 +0400

    Update install.sh

--- dnsmasq/install.sh
+++ dnsmasq/install.sh
@@ -4,7 +4,7 @@ apt update
 unlink /etc/resolv.conf
 
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 
 apt install -y dnsmasq
 systemctl is-enabled dnsmasq
